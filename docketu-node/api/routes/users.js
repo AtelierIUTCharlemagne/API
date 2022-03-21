954a8f0b4817531a5216979e5d8e48bcd75567ba
Clément Boulet <clement.boulet60510@gmail.com>
2022-03-21 18:18:23 +0100

    Ajout de signin

--- docketu-node/api/routes/users.js
+++ docketu-node/api/routes/users.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex.js');
 const bcrypt = require('bcryptjs');
-//const jwt = require('jsonwebtoken')
-//const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const methodNotAllowed = require('../errors/methodNotAllowed.js');
 
@@ -47,43 +48,70 @@ router.route('/signup')
     .patch(methodNotAllowed)
     .delete(methodNotAllowed)
     .put(methodNotAllowed)
-    .post(async (req, res, next ) => {
+    .post(async (req, res, next) => {
         const { username, email, passwd } = req.body
         const password = await bcrypt.hash(passwd, 10);
         knex.from('user').insert(
             {
-                'username' : username,
+                'username': username,
                 'email': email,
-                'password' : password,
+                'password': password,
             }
         ).then(() => {
             res.status(201).json({
                 "user": {
-                    'username' : username,
+                    'username': username,
                     'email': email,
-                    'password' : password,
+                    'password': password,
                 }
             })
         })
-        .catch((err) => {
-            res.status(500).json({
-                "type": "error",
-                "error": 500,
-                "message": `Erreur de connexion à la base de données ` + err
-            });
-        })
+            .catch((err) => {
+                res.status(500).json({
+                    "type": "error",
+                    "error": 500,
+                    "message": `Erreur de connexion à la base de données ` + err
+                });
+            })
     })
     .get(methodNotAllowed)
 
-router.get('/', (req, res, next) => {
-  res.json({ message: "Welcome !" })
-});
+router.route('/signin')
+    .patch(methodNotAllowed)
+    .delete(methodNotAllowed)
+    .put(methodNotAllowed)
+    .post(async (req, res, next) => {
+        const { email, passwd } = req.body
+        knex.from('user').select('id_user', 'username', 'email', 'password', 'create_time', 'last_connection')
+            .where({
+                'email': email
+            }).first()
+            .then(async (user) => {
+                if (!user) { res.status(400).json({ error: "Invalid username or password", status: "error" }); return; }
 
-router.post('/', (req, res, next) => {
-  if (!req.body.message || req.body.message == "" || typeof req.body.message == undefined) {
-    return res.status(400).json("Missing message var in body")
-  }
-  res.json({ message: req.body.message });//hello world!
-});
+                if (! await bcrypt.compare(passwd, user.password)) { res.status(400).json({ error: "Invalid username or password", status: "error" }); return; }
+
+                const token = jwt.sign(
+                    {
+                        id: user.id_user,
+                        username: user.username,
+                        email : user.email,
+                        create_time : user.create_time,
+                    },
+                    JWT_SECRET,
+                );
+
+                res.status(200).json({ data: token, status: "ok" });
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    "type": "error",
+                    "error": 500,
+                    "message": `Erreur de connexion à la base de données ` + err
+                });
+            })
+    })
+    .get(methodNotAllowed)
+
 
 module.exports = router;
