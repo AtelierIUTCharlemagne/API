2d7f106dbc0da3409de5eca84c765fb4dba867eb
guillaume1415 <52198703+guillaume1415@users.noreply.github.com>
2022-03-22 16:01:02 +0100

    annex_events all ok

--- docketu-node/service_gateway/app.js
+++ docketu-node/service_gateway/app.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const eventsRouter = require('./routes/events');
+const eventAnnexRouter = require('./routes/events_annex');
 
 const app = express();
 app.use(cors());
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
+app.use('/events_annex', eventsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
