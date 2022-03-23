2282aff166d14821a18e983214d16ce47071041c
guillaume1415 <52198703+guillaume1415@users.noreply.github.com>
2022-03-23 14:52:20 +0100

    feature get all users event ok feature get all users one event ok

--- docketu-node/service_events/routes/events_annex.js
+++ docketu-node/service_events/routes/events_annex.js
@@ -43,47 +43,133 @@ router.route('/')
 
     })
 
-// router.route('/create')
+
+
+
+/**
+ * Route : /events-annex/id (id d'un event)
+ * Méthode : GET
+ * Description : Retourne tous les annex lien a cet evenemnt
+ * retour : JSON de l'evenement
+ */
+router.route('/:id')
+    .patch(methodNotAllowed)
+    .delete(methodNotAllowed)
+    .post(methodNotAllowed)
+    .put(methodNotAllowed)
+    .get(function (req, res, next) {
+        knex.from('events_annex')
+            .select('*')
+            .where({
+                'events_id_events': req.params.id
+            })
+            .then((event) => {
+                if (event == null) {
+                    res.status(404).json({
+                        "type": "error",
+                        "error": 404,
+                        "message": `ressources non disponibles`
+                    });
+                } else {
+                    res.status(200).json(event)
+                }
+            }).catch((err) => {
+            res.status(500).json({
+                "type": "error",
+                "error": 500,
+                "message": `Erreur de connexion à la base de données ` + err
+            });
+        })
+    })
+
+// function insertAnswer(res, pseudo, present, user_id_user, events_id_events) {
+//     knex.from('events_annex').insert(
+//         {
+//             'pseudo': pseudo,
+//             'present': present,
+//             'user_id_user': user_id_user,
+//             'events_id_events': events_id_events
+//         }
+//     ).then(() => {
+//         res.status(201).json({
+//             "message": "created"
+//         })
+//     }).catch((err) => {
+//         res.status(500).json({
+//             "type": "error",
+//             "error": 500,
+//             "message": `Erreur de connexion à la base de données ` + err
+//         });
+//     })
+// }
+
+
+
+
+/*----------------------- test 2 */
+
+// router.route('/:id')
 //     .patch(methodNotAllowed)
 //     .delete(methodNotAllowed)
 //     .put(methodNotAllowed)
-//     .post(async (req, res, next) => {
-//         const { title, address, localisation, date_events, user_id_user } = req.body
-//         const token = uuidv4();
-//         const last_update = knex.fn.now();
-//         knex.from('events').insert(
-//             {
-//                 'title': title,
-//                 'address': address,
-//                 'localisation': localisation,
-//                 'token': token,
-//                 'date_events': date_events,
-//                 'last_update': last_update,
-//                 'user_id_user': user_id_user,
-//             }
-//         ).then(() => {
-//             res.status(201).json({
-//                 "event": {
-//                     'title': title,
-//                     'address': address,
-//                     'localisation': localisation,
-//                     'token': token,
-//                     'date_events': date_events,
-//                     'user_id_user': user_id_user,
-//                 }
-//             })
-//         })
-//             .catch((err) => {
+//     .post(methodNotAllowed)
+//     .get(function (req, res, next) {
+//         if (req.query.user_id) {
+//             knex.from('events_annex')
+//                 .select('*')
+//                 .where({
+//                     'events_id_events': req.query.user_id
+//                 })
+//                 .then((events) => {
+//                     if (events == null) {
+//                         res.status(404).json({
+//                             "type": "error",
+//                             "error": 404,
+//                             "message": `ressources non disponibles`
+//                         });
+//                     } else {
+//                         let liste_events =
+//                             {
+//                                 type: "collection",
+//                                 count: events.length,
+//                                 events: events
+//                             }
+//                         res.status(200).json(liste_events)
+//                     }
+//                 }).catch((err) => {
 //                 res.status(500).json({
 //                     "type": "error",
 //                     "error": 500,
 //                     "message": `Erreur de connexion à la base de données ` + err
 //                 });
 //             })
+//         } else {
+//             knex.from('events')
+//                 .select('*')
+//                 .then((events) => {
+//                     if (events == null) {
+//                         res.status(404).json({
+//                             "type": "error",
+//                             "error": 404,
+//                             "message": `ressources non disponibles`
+//                         });
+//                     } else {
+//                         let liste_events =
+//                             {
+//                                 type: "collection",
+//                                 count: events.length,
+//                                 events: events
+//                             }
+//                         res.status(200).json(liste_events)
+//                     }
+//                 }).catch((err) => {
+//                 res.status(500).json({
+//                     "type": "error",
+//                     "error": 500,
+//                     "message": `Erreur de connexion à la base de données ` + err
+//                 });
+//             })
+//         }
 //     })
-//     .get(methodNotAllowed)
-
-
-
 
 module.exports = router;
